d35947b03c857fb12ae1cc6f51ca347feb607a50
sunjingwen <453129077@qq.com>
2020-08-11 17:37:29 +0800

    es6完结篇

---
+++ es6/babel-demo/test.js
@@ -0,0 +1,6 @@
+let aa = '123';
+let fn1 = () => {
+  setTimeout(() => {
+    console.log(666);
+  }, 1000);
+}
---
+++ es6/babel-demo/test1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var aa = '123';
+var fn1 = function fn1() {
+  setTimeout(function () {
+    console.log(666);
+  }, 1000);
+};
