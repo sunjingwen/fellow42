f7268b869ee9c148be07297c8735815783909cb4
sunjingwen <453129077@qq.com>
2020-08-28 12:04:09 +0800

    react全家桶

---
+++ react-project/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// 用来做资源缓存，只在线上环境生效
+// serviceWorker.unregister();
